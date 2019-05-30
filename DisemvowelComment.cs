62f9d238160a9d7f3254391dee06c067732eb592
StephanieK <stephanie.kitchen@serko.com>
2019-05-30 17:39:07 +1200

    Refactoring to make the RemoveAllVowelsFrom method into smaller more readable parts

--- DisemvowelComment.cs
+++ DisemvowelComment.cs
@@ -6,14 +6,22 @@ class DisemvowelComment
 {
     public void RemoveAllVowelsFrom(List<string> trollComments)
     {
-        string vowels = "aeiou";
+        char[] vowels = {'a','e','i','o','u'};
+        //string vowels ="aeiou";
 
         List<string> alteredComments = new List<string>();
 
         foreach(var comment in trollComments)
         {
-            string temp = new string(comment.Where(x => !vowels.Contains(x)).ToArray());
-            alteredComments.Add(temp);
+            string newComment = comment.Replace('a'.ToString(), "");
+            newComment = newComment.Replace('e'.ToString(), "");
+            newComment = newComment.Replace('i'.ToString(), "");
+            newComment = newComment.Replace('o'.ToString(), "");
+            newComment = newComment.Replace('u'.ToString(), "");
+
+            //string newComment = new string(comment.Where(x => !vowels.Contains(x)).ToArray());
+            
+            alteredComments.Add(newComment);
         }
 
         PrintDisemvoweledComment(alteredComments);
@@ -23,7 +31,7 @@ class DisemvowelComment
     {
         foreach(string comment in commentsList)
         {
-            Console.WriteLine($"Your troll's comment has now been reduced to: \"{comment}\"");
+            Console.WriteLine($"Your troll's comment has now been reduced to: \"{comment}\"");      // return this to console
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -16,6 +16,4 @@ namespace Disemvowel
             remover.RemoveAllVowelsFrom(trollComments);
         }
     }
-
-
 }
